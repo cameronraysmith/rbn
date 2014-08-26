0e78d29b3cc088afcfebf1db6863bf15d3b7bbde
Cameron Smith <camrn86@gmail.com>
2014-08-26 14:20:40 -0400

    begin implementation of hierarchical modular systems

--- randcoeff.jl
+++ randcoeff.jl
@@ -1,15 +1,16 @@
 # stable:    N*C*sigma^2 < 1 sqrt(N*C)*s < 1
 # unstable:  N*C*sigma^2 > 1 sqrt(N*C)*s > 1
 
+using Iterators
 
-function randsys(N::Integer,C::Float64,sigma::Float64)
+function randsys(N::Integer,L::Integer,C::Float64,sigma::Float64)
 
-  # Generate random system of size N x N
+  # Generate random system of size N x M
   # with connectivity C
   # and m_ii = -1
-  M = zeros(Float64,N,N)
-  inds = randperm(N^2)[1:round(C*N^2)]
-  M[inds] = (sigma)*randn(int(round(C*N^2)))
+  M = zeros(Float64,N,L)
+  inds = randperm(N*L)[1:round(C*N*L)]
+  M[inds] = (sigma)*randn(int(round(C*N*L)))
 
   # for i = 1:N
   #   M[i,i] = -1.0
@@ -18,6 +19,76 @@ function randsys(N::Integer,C::Float64,sigma::Float64)
   return M
 
 end
+
+function randsys(N::Integer,C::Float64,sigma::Float64)
+  randsys(N,N,C,sigma)
+end
+
+function connectmods(modarray::Array{Array{Float64,2},1},C::Float64,sigma::Float64)
+  L = length(modarray)
+  modulepairs = Iterators.product(1:L,1:L)
+  n = map(x->size(x,1),modarray) # array of nodes in each module
+  N = sum(n)
+  M = zeros(Float64,N,N)
+
+  for(i,j) in modulepairs
+    if i==j
+      M[(n[i]*(i-1)+1):(n[i]*i),(n[j]*(j-1)+1):(n[j]*j)] = modarray[i]
+    else
+      M[(n[i]*(i-1)+1):(n[i]*i),(n[j]*(j-1)+1):(n[j]*j)] = randsys(n[i],n[j],C,sigma)
+    end
+  end
+
+  return M
+end
+
+function randhiermodsys(n::Integer,C::Float64,sigma::Float64,m::Integer,h::Integer,r::Float64)
+  # N - number of nodes at level 0
+  # m - number of modules at level 1
+  # h - number of hierarchical levels
+  # r - \frac{\rho_{h+1}}{\rho_h} ratio
+
+  L = 2^(h-1)*m # total number of modules
+  N = L*n # total number of nodes
+  M = zeros(Float64,N,N)
+
+  rhovec = zeros(Float64,h+1)
+  rhovec[1] = C
+  for i = 2:length(rhovec)
+    rhovec[i] = rhovec[i-1]*r
+  end
+
+  modvec = zeros(Integer,h+1)
+  modvec[1] = m
+  modvec[2:end] = 2
+
+  modarray = [[randsys(n,rhovec[1],sigma) for j in 1:m] for k in 1:2^(h-1)]
+
+  connectmods(vcat(
+                connectmods()
+              ))
+
+  # for i in 1:h
+
+
+
+  # for i = 2:h
+  #   modarray = [connectmods(modarray,rhovec[i]) for j in 1:modvec[i]]
+  # end
+
+  # modulepairs = Iterators.product(1:L,1:L)
+
+  # for (i,j) in modulepairs
+  #   if i==j
+  #     M[(n*(i-1)+1):n*i,(n*(j-1)+1):n*j] = randsys(n,rhovec[1],sigma)
+  #   else
+  #     M[(n*(i-1)+1):n*i,(n*(j-1)+1):n*j] = randsys(n,rhovec[2],sigma)
+  #   end
+  # end
+
+  # return M
+end
+
 
 function sysstab(M::Array{Float64,2})
 
@@ -40,24 +111,24 @@ function sysstab(M::Array{Float64,2})
 
 end
 
-T = 1000
-N = 40
-C = .0441
-# standard deviation sigma, variance sigma^2
-sigma = 0.5
+# T = 1000
+# N = 40
+# C = .0441
+# # standard deviation sigma, variance sigma^2
+# sigma = 0.5
 
-discspec = zeros(Float64,T)
-contspec = zeros(Float64,T)
+# discspec = zeros(Float64,T)
+# contspec = zeros(Float64,T)
 
-for t = 1:T
-  discspec[t],contspec[t] = sysstab(randsys(N,C,sigma))
-end
+# for t = 1:T
+#   discspec[t],contspec[t] = sysstab(randsys(N,C,sigma))
+# end
 
-println("NCsigma^2: $(N*C*sigma^2)")
-println("sqrt(NC)sigma: $(sqrt(N*C)*sigma)")
-println("mean spectral radius: $(mean(discspec))")
-msr = length(find(x->x>1,discspec))/T
-println("proportion with spectral radius greater than 1: $msr")
-mrp = length(find(x->x>0,contspec))/T
-println("mean maximum real part: $(mean(contspec))")
-println("proportion with real part greater than 0: $mrp")
+# println("NCsigma^2: $(N*C*sigma^2)")
+# println("sqrt(NC)sigma: $(sqrt(N*C)*sigma)")
+# println("mean spectral radius: $(mean(discspec))")
+# msr = length(find(x->x>1,discspec))/T
+# println("proportion with spectral radius greater than 1: $msr")
+# mrp = length(find(x->x>0,contspec))/T
+# println("mean maximum real part: $(mean(contspec))")
+# println("proportion with real part greater than 0: $mrp")
